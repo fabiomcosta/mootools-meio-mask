132139d59c86f57907a9fecb1400eacbfdab71d1
Fabio M. Costa <fabio@macfabio.(none)>
2009-09-20 21:28:34 -0300

    refactored the code a little, it looks better now, but still not finished.

--- source/moo.meio.mask.Mask.Reverse.js
+++ source/moo.meio.mask.Mask.Reverse.js
@@ -8,24 +8,35 @@ Meio.Mask.Reverse = new Class({
 		symbol: '',
 		precision: 2,
 		decimal: ',',
-		thousands: '.'
+		thousands: '.',
+		maxLength: 19
 		//decimal: true
 		//signal: false
     },
 
     initialize: function(element, options){
         this.parent(element, options);
+        var escapedDecimalChar = this.options.decimal.escapeRegExp(),
+            thousandsChar = this.options.thousands;
         if(this.options.alignText) this.element.setStyle('text-align', 'right');
-        this.thousandRegex = /(\d+)(\d{3})/;
-        //this.initialCharsRegex = new RegExp('^' + this.options.symbol.escapeRegExp());
+        this.maxlength = this.maxlength || this.options.maxLength;
+        this.thousandsRegex = /(\d+)(\d{3})/;
+        this.thousandsReplaceStr = '$1' + thousandsChar + '$2';
+        this.thousandReplaceRegex = new RegExp(thousandsChar.escapeRegExp(), 'g');
+        this.moveRightRegex = new RegExp('([' + escapedDecimalChar + '])(\\d)', 'g');
+        this.moveLeftRegex = new RegExp('(\\d)([' + escapedDecimalChar + '])', 'g');
+        var elementValue = this.element.get('value');
+        if(elementValue === ''){
+            elementValue = this.mask(elementValue);
+    		this.element.set('value', elementValue).defaultValue = elementValue;
+        }
     },
     
     focus: function(e, o){
         var element = this.element,
             elValue = element.get('value'),
             symbol = this.options.symbol;
-        if(elValue.substring(0, symbol.length) !== symbol)
-            element.set('value', (elValue = symbol + elValue));
+        element.set('value', (elValue = this.getValue(elValue, true)));
         if(this.options.selectOnFocus)
             element.setRange(symbol.length, elValue.length);
         this.parent(e, o);
@@ -33,16 +44,29 @@ Meio.Mask.Reverse = new Class({
     
     blur: function(e, o){
         this.parent(e, o);
-        var symbol = this.options.symbol,
-            element = this.element,
-            elValue = element.get('value');
-        if(elValue.substring(0, symbol.length) === symbol)
-            element.set('value', elValue.substring(symbol.length));
+        var element = this.element;
+        element.set('value', this.getValue(element.get('value')));
     },
     
     keypress: function(e, o){
     	if(this.isIgnoreKey(e)) return true;
         e.preventDefault();
+    	var _char = String.fromCharCode(e.code),
+    	    elValueFull = this.element.get('value');
+
+    	elValueFull = elValueFull.substring(0, o.range.start) + (o.isRemoveKey? '': _char) +  elValueFull.substring(o.range.end);
+    	var elValue = this.getValue(elValueFull);
+	    
+	    if(o.isRemoveKey){
+	        elValue = elValue.substring(0, elValue.length - 1).replace(this.moveLeftRegex, '$2$1');
+	    }
+	    else{
+	        if(!(/^\d$/).test(_char)) return true;
+        	else if(elValue.length >= this.maxlength) return true;
+	        elValue = elValue.replace(this.moveRightRegex, '$2$1');
+	    }
+	    elValue = this.mask(elValue, true);
+    	this.element.set('value', elValue).setRange(elValue.length);
     	return true;
     },
 
@@ -50,30 +74,40 @@ Meio.Mask.Reverse = new Class({
         e.preventDefault();
 		var element = this.element;
 		    elValue = element.get('value');
-		element.set('value', (elValue = this.mask(elValue))).setRange(elValue.length);
+		element.set('value', (elValue = this.mask(elValue, true))).setRange(elValue.length);
         return true;
     },
     
-    mask: function(str){
-        var number = Number(str.substring(this.options.symbol.length));
-        str = '' + number.toFixed(this.options.precision);
-        str = str.replace('.', this.options.decimal);
-        return this.options.symbol + (this.options.thousands? this.maskThousand(str): str);
+    mask: function(str, withSymbol){
+        str = str || '0';
+        str = this.unmask(str).replace('.', this.options.decimal);
+        return this.getValue(this.maskThousands(str), withSymbol);
     },
     
     unmask: function(str){
-        var thousandChar = this.options.thousands,
-	        precision = this.options.precision,
-	        symbol = this.options.symbol;
-	    if(thousandChar) str = str.replace(thousandChar, '');
-	    if(symbol) str = str.substring(symbol.length);
-	    return (precision)? str.replace(this.options.decimal, '.').toFloat().toFixed(precision): str.toInt();
+	    return this.toNumber(this.getValue(str));
     },
     
-    maskThousand: function(str){
-        var replaceStr = '$1' + this.options.thousands + '$2';
-        while(this.thousandRegex.test(str)) {
-            str = str.replace(this.thousandRegex, replaceStr);
+    toNumber: function(str){
+        if(!isFinite(str)){
+            var thousandsChar = this.options.thousands,
+                decimalChar = this.options.decimal;
+    	    if(thousandsChar) str = str.replace(this.thousandReplaceRegex, '');
+    	    if(decimalChar) str = str.replace(decimalChar, '.');
+        }
+        return str.toFloat().toFixed(this.options.precision);
+    },
+    
+    getValue: function(str, withSymbol){
+        var symbol = this.options.symbol;
+        return (str.substring(0, symbol.length) === symbol)?
+            withSymbol? str: str.substring(symbol.length):
+            withSymbol? symbol + str: str;
+    },
+    
+    maskThousands: function(str){
+        if(this.options.thousands){
+            while(this.thousandsRegex.test(str)) str = str.replace(this.thousandsRegex, this.thousandsReplaceStr);
         }
         return str;
     }
@@ -85,5 +119,5 @@ Meio.Mask.createMasks('Reverse', {
 	'Decimal'			: { },
 	'DecimalUs'         : { thousands: ',', decimal: '.' },
 	'Reais'			    : { symbol: 'R$ ' },
-	'Dolar'			    : { symbol: 'US$ ', thousands: ',', decimal: '.' }
+	'Dollar'			    : { symbol: 'US$ ', thousands: ',', decimal: '.' }
 });
