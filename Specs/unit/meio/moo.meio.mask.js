835735d8ad01af3c417bf34c2f1e35c9d922e371
Fabio M. Costa <fabiomcosta@gmail.com>
2010-07-25 16:28:08 -0300

    Ive not been able to add syn events since JSSpec doesnt support async specs. I'll use Jasmine with the new mootools structure soon so it will be possible to make it.

--- Specs/unit/meio/moo.meio.mask.js
+++ Specs/unit/meio/moo.meio.mask.js
@@ -1,22 +1,22 @@
 
-describe('String.meiomask()', {
+describe('Unit Tests and some simple user emulated', {
 
 	'before all': function(){
 		input = new Element('input', {'type': 'text', 'maxlength': 10});
 	},
 	
 	'should set the size of the input': function(){
-		input.meiomask('Time', 'Fixed', {autoSetSize: true});
+		input.meiomask('Fixed', 'Time', {autoSetSize: true});
 		value_of(input.get('size'), 5);
 	},
 	
 	'should not set the size of the input': function(){
-		input.set('size', 10).meiomask('Time', 'Fixed', {autoSetSize: true});
+		input.set('size', 10).meiomask('Fixed', 'Time', {autoSetSize: true});
 		value_of(input.get('size'), 10);
 	},
 	
 	'should remove the maxlength when mask is set, and reset it when mask is removed': function(){
-		input.meiomask('Time', 'Fixed');
+		input.meiomask('Fixed', 'Time');
 		value_of(input.get('maxlength'), null);
 		input.erase('meiomask');
 		value_of(input.get('maxlength'), 10);
@@ -25,23 +25,23 @@ describe('String.meiomask()', {
 	'should set properly the placeholder on the input': function(){
 		var el = new Element('input', {'type': 'text'});
 		$(document.body).grab(el);
-		el.meiomask('Date', 'Fixed');
+		el.meiomask('Fixed', 'Date');
 		el.focus();
 		value_of(el.value).should_be('__/__/____');
 	},
 	
 	'should mask the value from the input that actually haves a value': function(){
 		var el = new Element('input', {'type': 'text', 'value': '12122000'});
-		el.meiomask('Date', 'Fixed');
+		el.meiomask('Fixed', 'Date');
 		value_of(el.value).should_be('12/12/2000');
 	},
 	
 	'should mask a string with the fixed mask type option': function(){
-		value_of('12121212'.meiomask('Date', 'Fixed')).should_be('12/12/1212');
+		value_of('12121212'.meiomask('Fixed', 'Date')).should_be('12/12/1212');
 	},
 	
 	'should mask a string with the reverse.dollar mask': function(){
-		value_of('12121212'.meiomask('reverse.dollar')).should_be('US$ 12,121,212.00');
+		value_of('12121212'.meiomask('reverse.dollar')).should_be('12,121,212.00');
 	},
 	
 	'should mask a string with the reverse.integer mask': function(){
@@ -53,7 +53,7 @@ describe('String.meiomask()', {
 	},
 		
 	'should unmask the passed string': function(){
-		value_of('12/12/2000'.meiounmask('Date', 'Fixed')).should_be('12122000');
+		value_of('12/12/2000'.meiounmask('Fixed', 'Date')).should_be('12122000');
 	},
 	
 	'should unmask the passed string to a float number': function(){
